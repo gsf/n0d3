b05750e698247f9e65dfe19f37ae650a3750bab9
Gabriel Farrell <g@grrawr.com>
2012-12-26 10:32:42 -0500

    More clarity on plugins and clients

--- n0d3.js
+++ n0d3.js
@@ -2,17 +2,20 @@ function N0d3 () {
   this.plugins = [];
 }
 
-N0d3.prototype.join = function (joins) {
-  for (var key in joins) {
-    require('./adapters/'+key)(joins[key], this);
-  }
-};
-
 N0d3.prototype.log = require('util').log;
 
-N0d3.prototype.use = function () {
+N0d3.prototype.use = function (/* plugins */) {
   var args = Array.prototype.slice.call(arguments);
   this.plugins = this.plugins.concat(args);
+};
+
+N0d3.prototype.join = function (/* clients */) {
+  var n0d3 = this;
+  Array.prototype.forEach.call(arguments, function (client) {
+    n0d3.plugins.forEach(function (plugin) {
+      plugin(client, n0d3);
+    });
+  });
 };
 
 module.exports = function () {
